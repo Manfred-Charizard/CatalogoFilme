b5ea04d45820038faffac0aae51109cd1a291bfb
Manfred-Charizard <pedtogabriela@gmail.com>
2022-05-30 18:07:40 -0300

    pull

---
+++ 2. banco de dados/dml.sql
@@ -0,0 +1,65 @@
+USE FilmesInfoD;
+
+
+
+INSERT INTO tb_usuario (nm_usuario, ds_email, ds_senha)
+     VALUES ('admin', 'admin@admin.com.br', '1234');
+
+
+select id_usuario 		id,
+       nm_usuario		nome,
+       ds_email			email
+  from tb_usuario
+ where ds_email 		= 'admin@admin.com.br'
+   and ds_senha			= '1234';
+
+
+INSERT INTO tb_filme (id_usuario, nm_filme, ds_sinopse, vl_avaliacao, dt_lancamento, bt_disponivel)
+     VALUES (1, 'Harry Potter e a Camara Secreta', 'Filme bem tops', 8.2, '2012-02-11', true);
+
+
+UPDATE tb_filme 
+   SET img_filme     = '/storage/filme/asdfasdf.jp'
+ WHERE id_filme = 1;
+
+
+UPDATE tb_filme 
+   SET nm_filme      = 'Carros 3',
+       ds_sinopse    = 'filmes fodas',
+       vl_avaliacao  = 10,
+       dt_lancamento = '2019-02-09',
+       bt_disponivel = true
+ WHERE id_filme = 1;
+
+
+DELETE FROM tb_filme 
+      WHERE id_filme = 1;
+
+
+SELECT id_filme			id,
+	  nm_filme			nome,
+       vl_avaliacao		avaliacao,
+       dt_lancamento	lancamento,
+       bt_disponivel	disponivel
+  FROM tb_filme;
+
+
+SELECT id_filme			id,
+	  nm_filme			nome,
+       vl_avaliacao		avaliacao,
+       dt_lancamento	lancamento,
+       bt_disponivel	disponivel
+  FROM tb_filme
+ WHERE nm_filme			like '%a%';
+ 
+
+SELECT id_filme		id,
+	  nm_filme	     nome,
+       vl_avaliacao		avaliacao,
+	  ds_sinopse		sinopse,
+       dt_lancamento	lancamento,
+       bt_disponivel	disponivel,
+       img_filme         capa
+  FROM tb_filme
+ WHERE id_filme			= 1;
+
